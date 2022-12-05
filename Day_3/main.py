2e84b57418258af0ac4fb98e3763e05c9e21946b
Rob Newling <rnewling@gmail.com>
2022-12-04 20:48:30 -0500

    day 3 part 2 success

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,5 +1,14 @@
+class ElfGroup:
+    def __init__(self, group_rucksacks):
+        self.rucksacks = group_rucksacks
+
+    def find_badge(self):
+        return list(set(self.rucksacks[0].entire_inventory).intersection(self.rucksacks[1].entire_inventory, self.rucksacks[2].entire_inventory))[0]
+
+
 class Rucksack:
     def __init__(self, total_item_list):
+        self.entire_inventory = total_item_list
         self.compartment_a = [] # list of items
         self.compartment_b = [] # list of items
         self.divide_items_into_compartments(total_item_list)
@@ -25,24 +34,30 @@ class Item:
         else:
             print('Error. Invalid type.')
 
+
 def process_input(filename):
     rucksacks = []
     item_priorities = []
+    elf_groups_prep = []
+    elf_groups = []
 
     # load rucksacks
     with open(filename, 'r') as input_file:
         for line in input_file:
             rucksacks.append(Rucksack(line.strip()))
 
+    # form groups
+    elf_groups_prep = [rucksacks[i:i+3] for i in range(0, len(rucksacks), 3)]
+    for group in elf_groups_prep:
+        elf_groups.append(ElfGroup(group))
+
     # gather priorities
-    for rucksack in rucksacks:
-        item = Item(rucksack.find_item_appearing_in_both_compartments())
+    for group in elf_groups:
+        item = Item(group.find_badge())
         # print(f'Item {item.type} with priority {item.priority()}')
         item_priorities.append(item.priority())
 
     print(f'Sum of items: {sum(item_priorities)}')
-
-
 
 
 if __name__ == '__main__':
