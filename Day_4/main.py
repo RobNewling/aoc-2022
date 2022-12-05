e09aca911710135f5043094b70f99f8298517fd0
Rob Newling <rnewling@gmail.com>
2022-12-04 21:40:30 -0500

    day 4 part 1 success

---
+++ Day_4/main.py
@@ -0,0 +1,35 @@
+class AssignmentPair:
+    def __init__(self, section1, section2):
+        self.section1 = self.expand_section(section1)
+        self.section2 = self.expand_section(section2)
+
+    def expand_section(self, section):
+        start, stop = section.split('-')
+        return [i for i in range(int(start), int(stop)+1)]
+
+    def detect_subsets(self):
+        # true if one list is fully a subset of the other
+        if (set(self.section1).issubset(self.section2)
+                or set(self.section2).issubset(self.section1)):
+            return True
+        else:
+            return False
+
+
+def process_input(filename):
+    assignment_pairs = []
+    with open(filename, 'r') as input_file:
+        for line in input_file:
+            # print((line.strip().split(',')))
+            assignment_pairs.append(AssignmentPair(*line.strip().split(',')))
+
+    count = 0
+    for pair in assignment_pairs:
+        if pair.detect_subsets():
+            count += 1
+
+    print(f'Total pairs: {count}')
+
+
+if __name__ == '__main__':
+    process_input('input.txt')
